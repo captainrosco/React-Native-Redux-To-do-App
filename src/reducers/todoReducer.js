290a66bdd35063c76b4a6428680ac3fe7b43e8c4
Chad Danna <chad@formulated.io>
2017-07-24 19:14:53 -0600

    Update todoReducer • Update file name to be camelCase instead of under_score • Clean up formatting • Add a condition to prevent tasks to be added if blank

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,21 @@
+import {
+  ADD_TASK, TEXT_CHANGE, DELETE_TASK
+} from '../actions/'
+const INIT_STATE = { text: '', tasks: [] }
+
+export default function addTask (state = INIT_STATE, action) {
+  switch (action.type) {
+    case ADD_TASK:
+      if (state.text === '') return state
+      return { ...state, tasks: [...state.tasks, state.text], text: '' }
+      
+    case TEXT_CHANGE:
+      return { ...state, text: action.payload }
+
+    case DELETE_TASK:
+      const index = state.tasks.indexOf(action.payload)
+      return { ...state, tasks: [...state.tasks.splice(0, index), ...state.tasks.splice(1)] }
+    default:
+      return state
+  }
+}
