2daae253ba81756fc50682a321969aae26b65501
Chad Danna <chad@formulated.io>
2017-07-24 19:06:23 -0600

    Clean up App.js file formatting • Enable 'Component' from React

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import Router from './src/Router';
-import { Provider } from 'react-redux';
+import React, { Component } from 'react'
+import { AppRegistry } from 'react-native'
+import Router from './src/Router'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducers from './src/reducers';
+import reducers from './src/reducers'
 
-export default class App extends React.Component {
-  render() {
+export default class App extends Component {
+  render () {
     return (
       <Provider store={createStore(reducers)}>
         <Router />
       </Provider>
-    );
+    )
   }
 }
 
-
-AppRegistry.registerComponent('simpleapp', () => App);
+AppRegistry.registerComponent('simpleapp', () => App)
